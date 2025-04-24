3932c40d82b0fd96f5f5c7261af1a04cc11c654a
Minu Mathew <minumpmathews@gmail.com>
2025-04-24 16:49:21 -0500

    pgdump command

---
+++ pgdump.sh
@@ -0,0 +1,12 @@
+# requires postgresql@15
+
+# MacOS
+# brew install postgresql@15
+# brew info postgresql@15
+# check if postgres is in path. if not, add it to path.
+# echo 'export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"' >> ~/.bash_profile
+# source ~/.bash_profile
+# brew services start postgresql@15
+
+PGPASSWORD=${process.env.SUPABASE_PASSWORD} pg_dump -h aws-0-us-east-1.pooler.supabase.com -U postgres.${process.env.SUPABASE_URL} -d postgres --schema-only > schema.sql
+
