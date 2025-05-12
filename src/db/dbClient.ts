1aac1d2e5e63688a74ab17afc0ec8741de05d8b4
Minu Mathew <minumpmathews@gmail.com>
2025-05-12 17:39:39 -0500

    removed additional schema

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import * as keycloakSchema from './keycloakSchema';
+
 
 // Create postgres client
 const connectionString = process.env.NODE_ENV === 'development' 
@@ -10,17 +10,8 @@ const connectionString = process.env.NODE_ENV === 'development'
 
 const client = postgres(connectionString);
 
-// Create drizzle ORM instance for postgresDB
-export const db = drizzle(client, { schema });
-
-// Create keycloak client
-const keycloakConnectionString = process.env.KEYCLOAK_DATABASE_URL as string;
-const keycloakClient = postgres(keycloakConnectionString);
-
-// Create drizzle ORM instance for keycloakDB
-export const keycloakDB = drizzle(keycloakClient, { schema: keycloakSchema });
-
+// Create drizzle ORM instance for postgresDB with proper typing
+export const db = drizzle(client, { schema: schema });
 
 // Export all tables
 export * from './schema';
-export * from './keycloakSchema';
