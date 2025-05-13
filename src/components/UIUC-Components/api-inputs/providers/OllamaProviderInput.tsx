869a339726328fd6785ab8f60298110f4c1b3ad5
Chad Olson <chad@function49.com>
2025-05-13 16:15:57 -0500

    remove purple from LLM

--- src/components/UIUC-Components/api-inputs/providers/OllamaProviderInput.tsx
+++ src/components/UIUC-Components/api-inputs/providers/OllamaProviderInput.tsx
@@ -26,7 +26,7 @@ export default function OllamaProviderInput({
         shadow="sm"
         p="lg"
         radius="lg"
-        className="max-w-[330px] bg-[#15162c] md:w-[330px]"
+        className="max-w-[330px] bg-[--dashboard-background-dark] text-[--dashboard-foreground] md:w-[330px]"
       >
         <div
           style={{
@@ -71,7 +71,11 @@ export default function OllamaProviderInput({
                 }}
                 thumbIcon={
                   field.state.value ? (
-                    <IconCheck size="0.8rem" color="purple" stroke={3} />
+                    <IconCheck
+                      size="0.8rem"
+                      color="var(--dashboard-button)"
+                      stroke={3}
+                    />
                   ) : (
                     <IconX size="0.8rem" color="grey" stroke={3} />
                   )
@@ -79,11 +83,11 @@ export default function OllamaProviderInput({
                 styles={{
                   track: {
                     backgroundColor: field.state.value
-                      ? '#6a29a4 !important'
-                      : '#25262b',
+                      ? 'var(--dashboard-button) !important'
+                      : 'var(--dashboard-background-faded)',
                     borderColor: field.state.value
-                      ? '#6a29a4 !important'
-                      : '#25262b',
+                      ? 'var(--dashboard-button) !important'
+                      : 'var(--dashboard-background-faded)',
                   },
                 }}
               />
