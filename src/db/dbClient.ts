4ff27ffabfc61fd5101cc7dcad67c8d345891f8b
Chen Wang <cwang138@illinois.edu>
2025-05-19 15:29:08 -0500

    update use ssl to true; add more schema

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -2,11 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const host = process.env.NEXT_KEYCLOAK_RDS_ENDPOINT;
-const username = process.env.NEXT_KEYCLOAK_RDS_USERNAME;
-const password = process.env.NEXT_KEYCLOAK_RDS_PASSWORD;
+const host = process.env.KEYCLOAK_RDS_ENDPOINT;
+const username = process.env.KEYCLOAK_RDS_USERNAME;
+const password = process.env.KEYCLOAK_RDS_PASSWORD;
 const database = 'keycloak';
-const ssl = false;
+const ssl = true;
 
 // Build connection string
 const keycloakDBConnectionString = `postgres://${username}:${password}@${host}:5432/${database}${ssl ? '?ssl=require' : ''}`;
