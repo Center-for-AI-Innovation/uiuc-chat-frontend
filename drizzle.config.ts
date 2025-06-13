554db36b649181005ad8a3e35e505fd24dc46c40
Minu Mathew <minum@illinois.edu>
2025-06-13 13:41:43 -0500

    remove nodeenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from 'drizzle-kit';
 import 'dotenv/config';
 
-// Match the same logic from dbClient.ts
-const nodeEnv = process.env.NODE_ENV as string;
-const isLocalDevelopment = nodeEnv === 'localdevelopment';
 const connectionString = `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_ENDPOINT}/${process.env.POSTGRES_DATABASE}`;
 
 export default {
