645545b5deac3623b88c53260b4feb19a23b9def
Chen Wang <cwang138@illinois.edu>
2025-06-30 11:14:22 -0500

    mute select placeholder type error

--- src/components/Chat/PluginSelect.tsx
+++ src/components/Chat/PluginSelect.tsx
@@ -66,7 +66,8 @@ export const PluginSelect: FC<Props> = ({
         <select
           ref={selectRef}
           className="w-full cursor-pointer bg-transparent p-2"
-          // placeholder={t('Select a plugin') || ''}
+          // @ts-expect-error TODO: Fix select placeholder prop mismatch
+          placeholder={t('Select a plugin') || ''}
           value={plugin?.id || ''}
           onChange={(e) => {
             onPluginChange(
