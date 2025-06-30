d72a65bcc3cdde6e4d51affb7e5e304b95de2a77
Chen Wang <cwang138@illinois.edu>
2025-06-30 10:45:42 -0500

    downgrade react and react-dom; ts-ignore to bypass type error

--- src/components/UIUC-Components/DocGroupsTable.tsx
+++ src/components/UIUC-Components/DocGroupsTable.tsx
@@ -112,6 +112,7 @@ export function DocGroupsTable({ course_name }: { course_name: string }) {
     setDocumentGroupSearch(event.target.value)
   }
 
+  // @ts-ignore
   return (
     <>
       <GlobalStyle />
