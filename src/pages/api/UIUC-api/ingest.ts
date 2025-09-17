ea55283959f5299616e3a1552b0830a10d6ae27c
Max Burnette <mburnet2@illinois.edu>
2025-09-17 12:38:16 -0500

    add forceEmbeddings as optional parameter

--- src/pages/api/UIUC-api/ingest.ts
+++ src/pages/api/UIUC-api/ingest.ts
@@ -20,13 +20,15 @@ const handler = async (
       })
     }
 
-    const { uniqueFileName, courseName, readableFilename } = req.body
+    const { uniqueFileName, courseName, readableFilename, forceEmbeddings } =
+      req.body
 
     console.log(
       '👉 Submitting to ingest queue:',
       uniqueFileName,
       courseName,
       readableFilename,
+      forceEmbeddings,
     )
 
     if (!uniqueFileName || !courseName || !readableFilename) {
@@ -48,6 +50,7 @@ const handler = async (
         course_name: courseName,
         readable_filename: readableFilename,
         s3_paths: s3_filepath,
+        force_embeddings: forceEmbeddings,
       }),
     })
 
