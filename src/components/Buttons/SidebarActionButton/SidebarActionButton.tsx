a316542cefc551249cf0c5d152932b247c3d0015
Drshika Asher <67125579+drshika@users.noreply.github.com>
2025-09-24 15:50:27 -0400

    build issues

--- src/components/Buttons/SidebarActionButton/SidebarActionButton.tsx
+++ src/components/Buttons/SidebarActionButton/SidebarActionButton.tsx
@@ -3,12 +3,14 @@ import { type MouseEventHandler, type ReactElement } from 'react'
 interface Props {
   handleClick: MouseEventHandler<HTMLButtonElement>
   children: ReactElement
+  tooltip?: string
 }
 
-const SidebarActionButton = ({ handleClick, children }: Props) => (
+const SidebarActionButton = ({ handleClick, children, tooltip }: Props) => (
   <button
     className="min-w-[20px] p-1 text-[--foreground-faded] hover:text-[--foreground]"
     onClick={handleClick}
+    title={tooltip}
   >
     {children}
   </button>
