1e5e8e8be2c471da64fc5c00c83fae15badfce57
Chen Wang <cwang138@illinois.edu>
2025-12-03 11:22:50 -0600

    disable the drop (#465)

--- src/pages/api/home/home.tsx
+++ src/pages/api/home/home.tsx
@@ -27,7 +27,7 @@ import { selectBestTemperature } from '~/components/Chat/Temperature'
 import { LoadingSpinner } from '~/components/UIUC-Components/LoadingSpinner'
 import { MainPageBackground } from '~/components/UIUC-Components/MainPageBackground'
 import {
-  useFetchConversationHistory, useFetchLastConversation,
+  useFetchLastConversation,
   useUpdateConversation,
 } from '~/hooks/conversationQueries'
 import {
@@ -537,8 +537,6 @@ const Home = ({
     )
     dispatch({ field: 'tools', value: tools })
   }
-  const [isDragging, setIsDragging] = useState<boolean>(false)
-  const [dragEnterCounter, setDragEnterCounter] = useState(0)
 
   const GradientIconPhoto = () => (
     <svg
@@ -566,62 +564,6 @@ const Home = ({
       <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2" />
     </svg>
   )
-
-  // EFFECTS for file drag and drop --------------------------------------------
-  useEffect(() => {
-    const handleDocumentDragOver = (e: DragEvent) => {
-      e.preventDefault()
-    }
-
-    const handleDocumentDragEnter = (e: DragEvent) => {
-      setDragEnterCounter((prev) => prev + 1)
-      setIsDragging(true)
-    }
-
-    const handleDocumentDragLeave = (e: DragEvent) => {
-      e.preventDefault()
-      setDragEnterCounter((prev) => prev - 1)
-      if (dragEnterCounter === 1 || e.relatedTarget === null) {
-        setIsDragging(false)
-      }
-    }
-
-    const handleDocumentDrop = (e: DragEvent) => {
-      e.preventDefault()
-      setIsDragging(false)
-      setDragEnterCounter(0)
-    }
-
-    const handleDocumentKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        setIsDragging(false)
-        setDragEnterCounter(0)
-      }
-    }
-
-    const handleMouseOut = (e: MouseEvent) => {
-      if (!e.relatedTarget) {
-        setIsDragging(false)
-        setDragEnterCounter(0)
-      }
-    }
-
-    document.addEventListener('dragover', handleDocumentDragOver)
-    document.addEventListener('dragenter', handleDocumentDragEnter)
-    document.addEventListener('dragleave', handleDocumentDragLeave)
-    document.addEventListener('drop', handleDocumentDrop)
-    document.addEventListener('keydown', handleDocumentKeyDown)
-    window.addEventListener('mouseout', handleMouseOut)
-
-    return () => {
-      document.removeEventListener('dragover', handleDocumentDragOver)
-      document.removeEventListener('dragenter', handleDocumentDragEnter)
-      document.removeEventListener('dragleave', handleDocumentDragLeave)
-      document.removeEventListener('drop', handleDocumentDrop)
-      document.removeEventListener('keydown', handleDocumentKeyDown)
-      window.removeEventListener('mouseout', handleMouseOut)
-    }
-  }, [])
 
   useEffect(() => {
     if (window.innerWidth < 640) {
@@ -758,19 +700,6 @@ const Home = ({
             <Navbar isPlain={false} />
 
             <div className="flex h-full w-full overflow-y-auto sm:pt-0">
-              {isDragging &&
-                VisionCapableModels.has(
-                  selectedConversation?.model.id as OpenAIModelID,
-                ) && (
-                  <div
-                    className="absolute inset-0 z-10 flex h-full w-full flex-col items-center justify-center bg-[--background-dark] opacity-90">
-                    <GradientIconPhoto />
-                    <span className="text-3xl font-extrabold text-[--foreground]">
-                      Drop your image here!
-                    </span>
-                  </div>
-                )}
-
               <Chatbar
                 current_email={current_email}
                 courseName={course_name}
