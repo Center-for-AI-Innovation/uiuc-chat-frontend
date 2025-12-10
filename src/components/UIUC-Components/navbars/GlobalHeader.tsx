8e243c746367a4653b78363cb11534acf87bec81
ckouder <bingjiguo@icloud.com>
2025-12-10 12:31:36 -0800

    add alt to image

--- src/components/UIUC-Components/navbars/GlobalHeader.tsx
+++ src/components/UIUC-Components/navbars/GlobalHeader.tsx
@@ -304,6 +304,7 @@ export function LandingPageHeader({
         >
           <div style={{ width: '2.5rem', height: '2.5rem' }}>
             <img
+              alt="Illinois Logo"
               src="/media/logo_illinois.png"
               width="auto"
               height="100%"
