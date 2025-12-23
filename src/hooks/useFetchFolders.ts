11fe0ac05a16e6a19d06b79cb96162ff7e881efc
ckouder <bingjiguo@icloud.com>
2025-12-23 10:30:22 -0800

    refactor folder queries into smaller pieces

---
+++ src/hooks/useFetchFolders.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchFolders } from '~/utils/app/folders'
+
+export function useFetchFolders(user_email: string, course_name: string) {
+  return useQuery({
+    queryKey: ['folders', course_name],
+    queryFn: async () => (user_email ? fetchFolders(course_name) : []),
+    enabled: !!user_email,
+    refetchInterval: 20_000,
+  })
+}
