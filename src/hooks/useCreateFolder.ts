5c382da9a8c635b3ff58ca0afe6d5a459cfcfb02
ckouder <bingjiguo@icloud.com>
2025-12-23 10:30:22 -0800

    refactor folder queries into smaller pieces

---
+++ src/hooks/useCreateFolder.ts
@@ -0,0 +1,42 @@
+import { type QueryClient, useMutation } from '@tanstack/react-query'
+import {
+  type FolderWithConversation,
+  type FolderInterface,
+} from '~/types/folder'
+import { saveFolderToServer } from '~/utils/app/folders'
+
+export function useCreateFolder(
+  user_email: string,
+  queryClient: QueryClient,
+  course_name: string,
+) {
+  return useMutation({
+    mutationKey: ['createFolder', user_email, course_name],
+    mutationFn: async (newFolder: FolderWithConversation) =>
+      saveFolderToServer(newFolder, course_name),
+    onMutate: async (newFolder: FolderWithConversation) => {
+      await queryClient.cancelQueries({ queryKey: ['folders', course_name] })
+
+      queryClient.setQueryData(
+        ['folders', course_name],
+        (oldData: FolderInterface[]) => {
+          return [newFolder, ...oldData]
+        },
+      )
+
+      const oldFolders = queryClient.getQueryData(['folders', course_name])
+
+      return { newFolder, oldFolders }
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(['folders', course_name], context?.oldFolders)
+      console.error('Error saving updated folder to server:', error, context)
+    },
+    onSuccess: (data, variables, context) => {
+      // No need to do anything here because the folders query will be invalidated
+    },
+    onSettled: (data, error, variables, context) => {
+      queryClient.invalidateQueries({ queryKey: ['folders', course_name] })
+    },
+  })
+}
