45f64fdf635d09e57860351771e38f7fbae4c9df
rohan-uiuc <rohan13@illinois.edu>
2026-01-27 16:59:50 -0600

    test: relocate api tests

--- src/pages/api/home/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import home from '../index'
-
-describe('pages/api/home/index', () => {
-  it('re-exports Home component', () => {
-    expect(home).toBeTruthy()
-  })
-})
-
