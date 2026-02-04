e062f299c0b78df5e4f2ebb484d686446feb83fe
rohan-uiuc <rohan13@illinois.edu>
2026-02-03 19:55:43 -0600

    chore: add legacy re-export shims

---
+++ src/hooks/folderQueries.ts
@@ -0,0 +1,4 @@
+export { useCreateFolder } from './queries/useCreateFolder'
+export { useDeleteFolder } from './queries/useDeleteFolder'
+export { useFetchFolders } from './queries/useFetchFolders'
+export { useUpdateFolder } from './queries/useUpdateFolder'
