796ed842a31553ac825582f9c3c9a030c8bff756
Chen Wang <cwang138@illinois.edu>
2026-04-27 11:47:18 -0500

    refactor: remove unused userEvent import from N8NPage tests

--- src/components/UIUC-Components/__tests__/N8NPage.test.tsx
+++ src/components/UIUC-Components/__tests__/N8NPage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { describe, expect, it, vi } from 'vitest'
 import { screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { http, HttpResponse } from 'msw'
 
 import { server } from '~/test-utils/server'
